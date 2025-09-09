78db7570ce85f2aa7b4206ffff66e01ebf89f817
ItohanMomodu <dawala85@gmail.com>
2025-09-09 13:41:17 +0100

    Second push of ABCM project First version

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/**@type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#49504C",
+        secondary: "#5C9DF5",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
